d08a0ad013e1b224a1748c23d2fc9135bf520ed8
Larry Koontz <koontzevich.larry@gmail.com>
2024-03-09 13:35:38 +0200

    editing task

--- todo-app/app/components/AddTask.tsx
+++ todo-app/app/components/AddTask.tsx
@@ -39,7 +39,7 @@ const AddTask = () => {
               onChange={(e) => setNewTaskValue(e.target.value)}
               type="text"
               placeholder="Type here"
-              className="input input-bordered w-full w-full"
+              className="input input-bordered w-full"
             />
             <button type="submit" className="btn">
               Submit
